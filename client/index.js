57ddef521860b58d818f35814eb01693c75268eb
stefanos1316 <stefanos1316@gmail.com>
2019-01-29 14:06:05 +0000

    Added files upload feature on server

--- client/index.js
+++ client/index.js
@@ -1,7 +1,13 @@
 const request = require('superagent');
+const dirSearch = require('C:/Program Files (x86)/Common Files/Adobe/CEP/extensions/my_first_extension/client/dirSearch/dirDFS.js');
+const fs = require('fs');
 
 var agent = request.agent();
 var url = "http://127.0.0.1:3000";
+var timeout = 60000;
+var projectsInfo;
+var projectUuid;
+var version = 1
 
 function validate() {
   var username = document.getElementById("username").value;
@@ -18,7 +24,7 @@ function validate() {
     .query({
       org: 'allcancode'
     })
-    .timeout(60000)
+    .timeout(timeout)
     .send({
       username: username,
       password: password,
@@ -33,17 +39,19 @@ function validate() {
 }
 
 function fillDropDown(res) {
-
+  
   var getProjects = (agent
     .get(url + '/api/projects')
     .query({
       filter: 'all',
       components: true
     })
-    .timeout(60000)
+    .timeout(timeout)
     .then(res => {
       var select = document.getElementById("projects");
       var options = res.body.projects;
+      projectsInfo = res.body.projects;
+
       for (var i = 0; i < options.length; i++) {
         var opt = options[i].name;
         var el = document.createElement("option");
@@ -60,21 +68,62 @@ var openButton = document.querySelector("#analyze");
 openButton.addEventListener("click", analyzeImage);
 
 function analyzeImage() {
- // if (validate() == true) {
     csInterface.evalScript("bootstrap()", function (result) {
       uploadToServer(result);
     });
-  // } else {
-  //   return false;
-  // }
 }
 
 // This function is responsible to upload all the created images to the server
-function uploadToServer(result) {
+function uploadToServer(dirPath) {
   var projectName = document.getElementById("projects").value;
   if (projectName == "") {
     alert('No project was selected for uploading or user was not authendicated.');
   } else {
-    
+    dirSearch.dirDFS(dirPath, function (err, data) {
+      if (err) {
+        throw err;
+      }
+
+      // Find the project that is currently selected and get its uuid
+      for (var i = 0; i < projectsInfo.length; ++i) {
+        if(projectsInfo[i].name == projectName) {
+          projectUuid = projectsInfo[i].uuid;
+        }
+      }
+
+      // Create project asset under the project and then uploading them
+      data.forEach(function(entry) {
+        fileNameWithExtensions = entry.split("\\");
+        fileNameOnly = fileNameWithExtensions[fileNameWithExtensions.length - 1].split(".");
+
+        // API that creates project assets
+        var projectAsset = (agent
+          .post(url + '/api/projects/' + projectUuid + '/assets')
+          .timeout(timeout)
+          .send({
+              name: fileNameOnly[0],
+              description: fileNameOnly[0]
+          })
+          .then(res => {
+            var assetUuid = res.body.uuid;
+            var uploadFileToAsset = (agent
+              .post(url + '/api/projects/' + projectUuid + '/assets/' + assetUuid + '/file')
+              .timeout(timeout)
+              .attach(fileNameWithExtensions[fileNameWithExtensions.length - 1], entry)
+              .field('version', '0')
+              .then(res => {
+                var test = res.body;
+              })
+              .catch(err => {
+                alert('Error in uploading: ' + err.message);
+              }));
+          })
+          .catch(err => {
+            alert('Error in creating assets: ' + err.message);
+          }));
+      });
+      
+      alert('Files uploaded as assets in project' + projectName);   
+    });
   }
 }
